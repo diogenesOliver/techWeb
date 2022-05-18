1354a1b09db935a20035be29822116fb6da42ec9
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-05-18 13:21:34 -0300

    Version 1.0

---
+++ index.js
@@ -0,0 +1,8 @@
+let titleInfo = document.getElementById('container')
+
+let typed = new Typed(titleInfo, {
+    strings:['Diógenes Ribeiro de Oliveira', 'Desenvolvedor de Software', '20 anos de idade'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
