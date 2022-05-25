fe878c8261a51ded04f822c0c5b8ab235cf3e570
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-05-25 11:19:45 -0300

    Commit version - 1.3

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let titleInfo = document.getElementById('text')
+let exploreButton = document.getElementById('btn')
 
 let typed = new Typed(titleInfo, {
     strings:['JavaScript', 'NodeJS', 'TypeScript', 'ReactJS'],
     typeSpeed: 80,
     backSpeed: 80,
     loop: true
+})
+
+exploreButton.addEventListener('click', () => {
+    window.scroll({
+        top: window.innerHeight,
+        behavior: "smooth"
+    })
 })
