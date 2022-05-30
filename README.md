19471b3d020a333df9b226771655d61b5a7cd46a
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-05-30 13:50:22 -0300

    Commit version - 1.8

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>[PROJETO EM ANDAMENTO...]</h1>
+
+<p> O projeto consiste em criar a Home Page de uma plataforma de cursos online focada para entusiastas da programação </p>
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let exploreButton = document.getElementById('btn')
 
 let typed = new Typed(titleInfo, {
     strings:['JavaScript', 'NodeJS', 'TypeScript', 'ReactJS'],
-    typeSpeed: 80,
-    backSpeed: 80,
+    typeSpeed: 110,
+    backSpeed: 40,
     loop: true
 })
 
